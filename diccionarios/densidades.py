1196ce229f85099e4ee4e73d084522d69c7b8d14
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2016-09-21 21:35:57 -0500

    Agrega la presentación de diccionarios.

---
+++ diccionarios/densidades.py
@@ -0,0 +1,16 @@
+def read_densities(filename):
+    infile = open(filename, 'r')
+    densities = {}
+    for line in infile:
+        words = line.split()
+        density = float(words[-1])
+        if len(words[:-1]) == 2:
+            substance = words[0] + ' ' + words[1]
+        else:
+            substance = words[0]
+        densities[substance] = density
+    infile.close()
+    return densities
+
+densities = read_densities('densities.dat')
+print(densities)
