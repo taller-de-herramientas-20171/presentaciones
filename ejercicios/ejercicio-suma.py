ef615371467024edbb81a523e74d59ba0624117d
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2016-09-28 21:32:39 -0500

    Actualiza los comentarios de las funciones.

--- ejercicios/ejercicio-suma.py
+++ ejercicios/ejercicio-suma.py
@@ -2,6 +2,11 @@
 
 
 def sum(M):
+    """Se encarga de realizar la siguiente suma:
+
+    sum{0}{M}{3*i / (i**3 + 1)}
+
+    La suma anterior converge a 3.334"""
     acc = 0
     for i in range(1, M + 1):
         acc += 3 * i / (i**3 + 1)
@@ -9,6 +14,7 @@ def sum(M):
 
 
 def testSum():
+    """Verifica si la función sum está funcionando correctamente"""
     epsilon = 0.1
     paso_todo = True
     paso_todo = paso_todo and (3.334 - sum(100)) < epsilon
